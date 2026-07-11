5ddcbdc616dc6e1c9234ca3f27aa2a2156833828
Ryno <rjrfourie@outlook.com>
2026-07-11 14:04:19 +0200

    feat: adding descrement button

--- dom-merge-conflict/tasks/buttons-and-counter/src/header.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -5,6 +5,7 @@ export function Header() {
     header.innerHTML = `
         <h1>Number Counter</h1>
         <p>A simple counter. Press increment to increase the count by one.</p>
+        <p>A simple counter. Press decrement to decrease the count by one.</p>
     `;
 
     return header;
